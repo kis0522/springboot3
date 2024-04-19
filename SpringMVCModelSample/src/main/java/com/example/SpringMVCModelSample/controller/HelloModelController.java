4acc5ca0a262a7684909ba4ec47b7662c8b4740e
kis0522 <kiminsu0522@naver.com>
2024-04-19 11:48:31 +0900

    SpringMVCModelSample

---
+++ SpringMVCModelSample/src/main/java/com/example/SpringMVCModelSample/controller/HelloModelController.java
@@ -0,0 +1,19 @@
+package com.example.SpringMVCModelSample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")
+public class HelloModelController {
+    //@GetMapping("bbb")
+    @GetMapping("model")
+    public String helloView(Model model){
+        model.addAttribute("msg","타임리프!!!");
+        model.addAttribute("name","길동");
+        //return "helloThymeleaf";
+        return "useThymeleaf";
+    }
+}
